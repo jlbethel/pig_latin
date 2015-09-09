ccd530dd86127cfb78891aa7ff4fc645d32771f4
Deron Johnson & Jason Bethel <student@epicodus.com>
2015-09-09 09:57:38 -0700

    rewrite first test/change fucntion to pass spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 var pigLatin = function(userInput) {
+
   var inputArray = userInput.split('');
-  var firstLetter = inputArray.splice(0,1);
-  inputArray.push(firstLetter);
+  if (inputArray[0] === "a" || "e" || "i" || "o" || "u") {
+    inputArray.push('ay');
+  }
+  // var firstLetter = inputArray.splice(0,1);
+  // inputArray.push(firstLetter);
+
   var pigString = inputArray.join('');
 
   return pigString;
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe('pigLatin', function() {
-  it("moves first letter to the end of word", function() {
-    expect(pigLatin("pig")).to.equal("igp");
+  it("adds ay to end of word if word begins with vowel", function() {
+    expect(pigLatin("any")).to.equal("anyay");
   });
+
+  // it("moves first letter plus ay to end of array", function() {
+  //   expect(pigLatin("pig")).to.equal("igpay");
+  // });
+
+
 });
