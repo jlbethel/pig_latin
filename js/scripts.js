b01544cf9deeb988f74feec83501b636a4d9b7f8
Deron Johnson & Jason Bethel <student@epicodus.com>
2015-09-09 14:13:56 -0700

    write/pass test for sentences

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
 var pigLatin = function(userInput) {
 
-  var inputArray = userInput.split('');
-  var inputLength = userInput.length;
+  var inputPhrase = userInput.split(' ');
+  var inputPhraseLength = inputPhrase.length;
+  var pigPhrase = [];
+  for (var index = 0; index < inputPhraseLength; index++) {
+    var inputArray = inputPhrase[index].split('');
+    var inputLength = inputArray.length;
 
-  if (inputArray[0] === ("y")) {
-    var letterY = inputArray.splice(0, 1);
-    inputArray = inputArray.concat(letterY, "ay");
-  } else {
-    for (var i = 0; i < inputLength; i++) {
-      if (inputArray[0].match(/([aeiouy])/g)) {
-        inputArray.push('ay');
-        break;
-      } else if (inputArray[0] === ("q") && inputArray[1] === ("u")) {
-        var currentLetters = inputArray.splice(0, 2);
-        inputArray = inputArray.concat(currentLetters);
-      } else {
-        var currentLetter = inputArray.splice(0, 1);
-        inputArray.push(currentLetter);
+    if (inputArray[0] === ("y")) {
+      var letterY = inputArray.splice(0, 1);
+      inputArray = inputArray.concat(letterY, "ay");
+    } else {
+      for (var i = 0; i < inputLength; i++) {
+        if (inputArray[0].match(/([aeiouy])/g)) {
+          inputArray.push('ay');
+          break;
+        } else if (inputArray[0] === ("q") && inputArray[1] === ("u")) {
+          var currentLetters = inputArray.splice(0, 2);
+          inputArray = inputArray.concat(currentLetters);
+        } else {
+          var currentLetter = inputArray.splice(0, 1);
+          inputArray.push(currentLetter);
+        }
       }
     }
+    var pigString = inputArray.join('');
+    pigPhrase.push(pigString);
   }
-  var pigString = inputArray.join('');
 
-  return pigString;
+  var outputPhrase = pigPhrase.join(' ');
+
+
+  return outputPhrase;
 };
